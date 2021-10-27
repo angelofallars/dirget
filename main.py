8e4b2f0aff011c71b85786cb2ee5cefc3c773fba
Angelo Fallaria <ba.fallaria@gmail.com>
2021-10-27 17:36:46 +0800

    Add detection and usage of .gitignore

--- main.py
+++ main.py
@@ -14,6 +14,31 @@ def path_has_hidden_dir(directory: str) -> bool:
         return False
 
 
+# Return the specifications in the .gitignore file if it exists,
+# otherwise return none
+def get_git_ignore(directory: str) -> list[str] | None:
+    git_ignore_path = os.path.join(directory, ".gitignore")
+    git_ignore = []
+
+    if os.path.exists(git_ignore_path):
+        print(f".gitignore detected: {git_ignore_path}")
+        git_ignore_raw = open(git_ignore_path).readlines()
+
+        for i in range(len(git_ignore_raw)):
+            # Remove blank lines
+            if git_ignore_raw[i] != "\n":
+                git_ignore.append(git_ignore_raw[i])
+
+        # Remove the ending '\n' from entries
+        for i in range(len(git_ignore)):
+            git_ignore[i] = git_ignore[i][:-2]
+
+        return git_ignore
+
+    else:
+        return None
+
+
 def main() -> int:
     current_dir = "./"
 
@@ -27,21 +52,39 @@ def main() -> int:
             # Go up one directory
             current_dir += "../"
 
-        if os.getcwd() == '/':
+        if os.path.abspath(current_dir) == '/':
             stderr.write("ERROR: You are not in a git repository.\n")
             return 1
 
+    # Get the .gitignore file if it exists
+    git_ignore = get_git_ignore(current_dir)
+
     # List all files from the current git directory recursively
     for pwd, dirs, files in os.walk(current_dir, topdown=False):
-        # TODO - Ignore files listed in .gitignore
 
         if path_has_hidden_dir(pwd):
             continue
 
         for name in files:
+            # [2:] index to remove ./ in start of file
+            current_file_dir = os.path.join(pwd, name)[2:]
+            file_is_hidden = False
+
+            # Ignore files listed in .gitignore
+            if git_ignore is not None:
+                for ignore_file in git_ignore:
+                    if ignore_file in current_file_dir:
+                        print(f"Hidden file: {current_file_dir}\n \
+ Ignore file was '{ignore_file}'")
+                        file_is_hidden = True
+                        break
+
+                if file_is_hidden:
+                    continue
+
             # Ignore hidden files that start with .
             if name[0] != ".":
-                print(os.path.join(pwd, name)[2:])  # [2:] index to remove ./
+                print(current_file_dir)
 
     return 0
 
