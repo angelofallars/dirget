60eb2bf496ac92e7925ba9652b239c5f03997d0c
Angelo Fallaria <ba.fallaria@gmail.com>
2021-10-27 16:49:13 +0800

    Refactor code into a main() function

--- main.py
+++ main.py
@@ -3,35 +3,48 @@
    then list the files with `ls -R`"""
 import os
 import re
-
-mypath = "./"
+from sys import stderr
 
 
 def path_has_hidden_dir(directory: str) -> bool:
     hidden_dir_regex = re.compile(r".*/\.[^/\.].*")
-
     if hidden_dir_regex.match(directory):
         return True
     else:
         return False
 
 
-# Search up until a folder with .git is found
-while True:
-    current_dir_files = os.listdir(mypath)
+def main() -> int:
+    current_dir = "./"
 
-    if ".git" in current_dir_files:
-        break
-    else:
-        # Go up one directory
-        mypath += "../"
+    # Search up until a folder with .git is found
+    while True:
+        current_dir_files = os.listdir(current_dir)
+
+        if ".git" in current_dir_files:
+            break
+        else:
+            # Go up one directory
+            current_dir += "../"
+
+        if os.getcwd() == '/':
+            stderr.write("ERROR: You are not in a git repository.\n")
+            return 1
+
+    # List all files from the current git directory recursively
+    for pwd, dirs, files in os.walk(current_dir, topdown=False):
+        # TODO - Ignore files listed in .gitignore
+
+        if path_has_hidden_dir(pwd):
+            continue
+
+        for name in files:
+            # Ignore hidden files that start with .
+            if name[0] != ".":
+                print(os.path.join(pwd, name)[2:])  # [2:] index to remove ./
+
+    return 0
 
 
-# List all files from the current git directory recursively
-for pwd, dirs, files in os.walk(mypath, topdown=False):
-    if path_has_hidden_dir(pwd):
-        continue
-
-    for name in files:
-        if name[0] != ".":
-            print(os.path.join(pwd, name)[2:])  # Start from 2: to remove ./
+if __name__ == "__main__":
+    main()
